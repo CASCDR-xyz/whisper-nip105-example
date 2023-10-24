e85ae1af57d3e88634fcd98b5ae29d73151ea22c
Jim Carucci <96802642+uncleJim21@users.noreply.github.com>
2023-10-24 16:32:51 -0400

    starting l402 cut over

--- app.js
+++ app.js
@@ -6,7 +6,280 @@ const util = require('util');
 const path = require('path');
 require('dotenv').config();
 
+const WebSocket = require("ws");
+const cors = require('cors');
+const axios = require("axios");
+const bolt11 = require("bolt11");
+const bodyParser = require("body-parser");
+const { getBitcoinPrice } = require('./lib/bitcoinPrice');
+const {
+  relayInit,
+  getPublicKey,
+  getEventHash,
+  getSignature,
+} = require("nostr-tools");
+const {
+  WHSPR_SCHEMA,
+  WHSPR_RESULT_SCHEMA,
+  OFFERING_KIND,
+} = require("./lib/defines.js");
+const { sleep } = require("./lib/helpers");
+
+global.WebSocket = WebSocket;
+
+const mongoose = require("mongoose");
+
+// --------------------- MONGOOSE -----------------------------
+
+const JobRequestSchema = new mongoose.Schema({
+  invoice: Object,
+  paymentHash: String,
+  verifyURL: String,
+  status: String,
+  result: String,
+  price: Number,
+  requestData: Object,
+  requestResponse: Object,
+  service: String,
+  state: String,
+});
+
+const JobRequest = mongoose.model("JobRequest", JobRequestSchema);
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB!");
+});
+
+// --------------------- HELPERS -----------------------------
+
+function logState(service, paymentHash, state) {
+  console.log(`${paymentHash.substring(0, 5)} - ${service}: ${state}`);
+}
+
+function getLNURL() {
+  const parts = process.env.LN_ADDRESS.split("@");
+  if (parts.length !== 2) {
+    throw new Error(`Invalid lnAddress: ${process.env.LN_ADDRESS}`);
+  }
+  const username = parts[0];
+  const domain = parts[1];
+  return `https://${domain}/.well-known/lnurlp/${username}`;
+}
+
+async function createNewJobDocument(service, invoice, paymentHash, price) {
+  const newDocument = new JobRequest({
+    invoice,
+    paymentHash,
+    verifyURL: invoice.verify,
+    price,
+    service,
+    status: "UNPAID",
+    result: null,
+    requestData: null,
+  });
+
+  // Save the document to the collection
+  await newDocument.save();
+}
+
+async function findJobRequestByPaymentHash(paymentHash) {
+  const jobRequest = await JobRequest.findOne({ paymentHash }).exec();
+  if (!jobRequest) {
+    throw new Error("No Doc found");
+  }
+
+  return jobRequest;
+}
+
+async function getIsInvoicePaid(paymentHash) {
+  const doc = await findJobRequestByPaymentHash(paymentHash);
+
+  const invoice = doc.invoice;
+
+  if (doc.status == "PAID") {
+    return { isPaid: true, invoice };
+  }
+
+  const response = await axios.get(doc.verifyURL, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const isPaid = response.data.settled == true;
+
+  doc.status = isPaid ? "PAID" : doc.status;
+  await doc.save();
+
+  return { isPaid, invoice };
+}
+
+async function getPaymentHash(invoice) {
+  const decodedInvoice = await bolt11.decode(invoice);
+  const paymentHashTag = decodedInvoice.tags.find(
+    (tag) => tag.tagName === "payment_hash"
+  ).data;
+  return paymentHashTag;
+}
+
+async function generateInvoice(service) {
+  const msats = await getServicePrice(service);
+  const lnurlResponse = await axios.get(getLNURL(), {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const lnAddress = lnurlResponse.data;
+
+  if (msats > lnAddress.maxSendable || msats < lnAddress.minSendable) {
+    throw new Error(
+      `${msats} msats not in sendable range of ${lnAddress.minSendable} - ${lnAddress.maxSendable}`
+    );
+  }
+
+  const expiration = new Date(Date.now() + 3600 * 1000); // One hour from now
+  const url = `${lnAddress.callback}?amount=${msats}&expiry=${Math.floor(
+    expiration.getTime() / 1000
+  )}`;
+
+  const invoiceResponse = await axios.get(url);
+  const invoiceData = invoiceResponse.data;
+
+  const paymentHash = await getPaymentHash(invoiceData.pr);
+  const successAction = getSuccessAction(service, paymentHash);
+
+  const invoice = { ...invoiceData, successAction, paymentHash };
+
+  await createNewJobDocument(service, invoice, paymentHash, msats);
+
+  return invoice;
+}
+
+async function getServicePrice(service) {
+  const bitcoinPrice = await getBitcoinPrice(); 
+  switch (service) {
+    case "WHSPR":
+      return usd_to_millisats(process.env.WHISPR_USD,bitcoinPrice);
+    default:
+      return usd_to_millisats(process.env.WHISPR_USD,bitcoinPrice);
+  }
+}
+
+function getSuccessAction(service, paymentHash) {
+  return {
+    tag: "url",
+    url: `${process.env.ENDPOINT}/${service}/${paymentHash}/get_result`,
+    description: "Open to get the confirmation code for your purchase.",
+  };
+}
+
+
+app.post("/:service", async (req, res) => {
+  try {
+    const service = req.params.service;
+    const invoice = await generateInvoice(service);
+    const doc = await findJobRequestByPaymentHash(invoice.paymentHash);
+
+    doc.requestData = req.body;
+    doc.state = "NOT_PAID";
+    await doc.save();
+
+    logState(service, invoice.paymentHash, "REQUESTED");
+
+    res.status(402).send(invoice);
+  } catch (e) {
+    console.log(e.toString().substring(0, 150));
+    res.status(500).send(e);
+  }
+});
+
+app.get("/:service/:payment_hash/get_result", async (req, res) => {
+  try {
+    const service = req.params.service;
+    const paymentHash = req.params.payment_hash;
+    const { isPaid, invoice } = await getIsInvoicePaid(paymentHash);
+
+    logState(service, paymentHash, "POLL");
+    if (isPaid != true) {
+      res.status(402).send({ ...invoice, isPaid });
+    } else {
+      const doc = await findJobRequestByPaymentHash(paymentHash);
+
+      switch (doc.state) {
+        case "WORKING":
+          logState(service, paymentHash, "WORKING");
+          res.status(202).send({ state: doc.state });
+          break;
+        case "ERROR":
+        case "DONE":
+          logState(service, paymentHash, doc.state);
+          res.status(200).send(doc.requestResponse);
+          break;
+        default:
+          logState(service, paymentHash, "PAID");
+          const data = doc.requestData;
+          submitService(service, data)
+            .then(async (response) => {
+              doc.requestResponse = response;
+              doc.state = "DONE";
+              await doc.save();
+            })
+            .catch(async (e) => {
+              doc.requestResponse = e;
+              doc.state = "ERROR";
+              await doc.save();
+            });
+
+          doc.state = "WORKING";
+          await doc.save();
+          res.status(202).send({ state: doc.state });
+      }
+    }
+  } catch (e) {
+    console.log(e.toString().substring(0, 300));
+    res.status(500).send(e);
+  }
+});
+
+app.get("/:service/:payment_hash/check_payment", async (req, res) => {
+  try {
+    const paymentHash = req.params.payment_hash;
+    const { isPaid, invoice } = await getIsInvoicePaid(paymentHash);
+
+    res.status(200).json({ invoice, isPaid });
+  } catch (e) {
+    console.log(e.toString().substring(0, 50));
+    res.status(500).send(e);
+  }
+});
+
+function submitService(service, data) {
+  switch (service) {
+    case "WHSPR":
+      return callWhisper(data);
+  }
+}
+
+async function callWhisper(data) {
+
+  try {
+    const response = await axios(config);
+    return response.data;
+  } catch (e) {
+    console.log(`ERROR: ${e.toString().substring(0, 50)}`);
+    return e;
+  }
+}
+
 // Promisify necessary functions
+app.use(cors());
 const writeFile = util.promisify(fs.writeFile);
 
 // Initialize app
