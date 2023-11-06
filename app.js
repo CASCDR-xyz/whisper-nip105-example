69e94429151e2d36703017fc67d8e5b993cc2547
Jim Carucci <96802642+uncleJim21@users.noreply.github.com>
2023-11-06 17:34:05 -0600

    try another hack...

--- app.js
+++ app.js
@@ -45,6 +45,15 @@ app.use(cors({
 }));
 
 app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin (*)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 const writeFile = util.promisify(fs.writeFile);
 
 app.use(bodyParser.json());
